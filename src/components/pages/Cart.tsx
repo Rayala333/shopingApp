ce1cf6833b43ea9adebb19bf54d32192370bd6c2
Your Name <you@example.com>
2023-12-19 12:44:41 +0530

    newcart

--- src/components/pages/Cart.tsx
+++ src/components/pages/Cart.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React from 'react';
+
+import { useAppDispatch,useAppSelecter } from '../redux/hooks';
+
 
 const Cart = () => {
+  const dispatch = useAppDispatch()
+
+  const products  = useAppSelecter((state)=>state.products)
+  const {loading,cart} = products;
+
+  console.log(cart,"cart")
+
+  console.log()
   return (
     <div>Cart</div>
   )
